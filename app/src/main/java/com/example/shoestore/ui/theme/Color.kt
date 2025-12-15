8344f994a425ca58e6b7b4237f9966cc7d0d4af1
Malev <nikitozzz.m2005@mail.ru>
2025-12-15 13:46:59 +0300

    Добавил цвета в Color.kt

--- app/src/main/java/com/example/shoestore/ui/theme/Color.kt
+++ app/src/main/java/com/example/shoestore/ui/theme/Color.kt
@@ -9,3 +9,22 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// ДОБАВЛЕННЫЕ ЦВЕТА В ПРОЕКТ
+val RedS  = Color(0xF87265)
+
+val Accent  = Color(0x48B2E7)
+
+val Disable  = Color(0x2B6B8B)
+
+val SubTextLight  = Color(0xD8D8D8)
+
+val Background  = Color(0xF7F7F9)
+
+val Block  = Color(0xFFFFFF)
+
+val Text  = Color(0x2B2B2B)
+
+val Hint  = Color(0x2B2B2B)
+
+val SubTextDark  = Color(0x707B81)
